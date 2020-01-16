cc324c917460c4a325d8b8ed3ae41fae12f7be52
Iury Fukuda <iuryfukuda@gmail.com>
2020-01-16 15:16:06 -0300

    add hook to add some configs when have x frame

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,11 +1,4 @@
-
-;; Botstrap
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-
+;; find all installed packages and activate them
 (package-initialize)
 
 (menu-bar-mode -1) ; remove menu bar
