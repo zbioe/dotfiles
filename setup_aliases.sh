e69f6e3c5c3009437e5cbafe26171f00b392693d
Iury Fukuda <iuryfukuda@gmail.com>
2020-02-05 17:11:23 -0300

    add i3status

--- setup_aliases.sh
+++ setup_aliases.sh
@@ -14,6 +14,7 @@ make_link $(pwd)/emacs.d ~/.emacs.d
 make_link $(pwd)/gitignore ~/.gitignore
 make_link $(pwd)/gitconfig ~/.gitconfig
 make_link $(pwd)/i3 ~/.config/i3
+make_link $(pwd)/i3status ~/.config/i3status
 make_link $(pwd)/fish ~/.config/fish
 make_link $(pwd)/bin ~/bin
 
