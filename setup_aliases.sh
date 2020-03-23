bfd372d0773507125a72842ad2d909693a8fca56
Iury Fukuda <iuryfukuda@gmail.com>
2020-03-23 16:12:21 -0300

    add theme to gtk

--- setup_aliases.sh
+++ setup_aliases.sh
@@ -18,5 +18,7 @@ make_link $(pwd)/i3 ~/.config/i3
 make_link $(pwd)/i3status ~/.config/i3status
 make_link $(pwd)/fish ~/.config/fish
 make_link $(pwd)/bin ~/bin
+make_link $(pwd)/gtk-3.0 ~/.config/gtk-3.0
+make_link $(pwd)/gtkrc-2.0 ~/.gtkrc-2.0
 
 echo "aliases created"
